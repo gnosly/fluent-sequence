38e0385928acbb5bcdd396bd66d42f773ecb7aac
gnosly <fabriziogiovannetti@gmail.com>
2018-04-08 12:26:40 +0200

    refactoring

--- src/main/scala/com/gnosly/fluentsequence/view/fixedwidth/FixedWidthFormatter.scala
+++ src/main/scala/com/gnosly/fluentsequence/view/fixedwidth/FixedWidthFormatter.scala
@@ -8,69 +8,69 @@ import scala.collection.mutable
 class FixedWidthFormatter(painter: FixedWidthPainter) {
 
 	def format(viewModel: ViewModelComponents): mutable.TreeMap[String, Fixed2DPoint] = {
-		val result = new PointMap
+		val pointMap = new PointMap
 
 		while (true) {
-
-			val iteration = result.toMap().toMap
-
-			for (actor <- viewModel._actors) {
-				val actorComponent = actor._2
-				if (actorComponent.id == 0) {
-					result.put(topLeftCornerIdForActor(actorComponent.id), Fixed2DPoint(LEFT_MARGIN, TOP_MARGIN))
-				} else {
-					result.put(topLeftCornerIdForActor(actorComponent.id),
-						result(topRightCornerIdForActor(actorComponent.id - 1)).right(DISTANCE_BETWEEN_ACTORS))
-				}
-
-				result.put(topRightCornerIdForActor(actorComponent.id),
-					result(topLeftCornerIdForActor(actorComponent.id)).right(painter.preRender(actorComponent).x))
-
-				val actorBox = painter.preRender(actorComponent)
-
-				result.put(bottomMiddleCornerIdForActor(actorComponent.id),
-					result(topLeftCornerIdForActor(actorComponent.id))
-						.right((actorBox.x - 1) / 2)
-						.down(actorBox.y)
-				)
-
-				for (activity <- actorComponent.activities) {
-					if (activity.id == 0) {
-						result.put(topLeftCornerIdForActivity(actorComponent.id, activity.id),
-							result(bottomMiddleCornerIdForActor(actorComponent.id)).left(painter.preRender(activity)))
-						for (point <- activity.rightPoints) {
-							point._2 match {
-								case autoSignal: AutoSignalComponent => {
-									result.put("actor_0_activity_0_right_point_0", Fixed2DPoint(6, 6))
-									result.put("actor_0_activity_0_right_point_1", Fixed2DPoint(6, 9))
-								}
-								case biSignal: BiSignalComponent => {
-									result.put("actor_0_activity_0_right_point_0", Fixed2DPoint(6, 6))
-								}
-							}
-
-						}
-
-						for (point <- activity.leftPoints) {
-							point._2 match {
-								case biSignal: BiSignalComponent => {
-									result.put(pointForActivity(actorComponent.id
-										, activity.id, point._1, "left"),
-										Fixed2DPoint(19, 6))
-								}
-							}
-
-						}
-					}
-				}
-			}
-
-			if (result.toMap().toMap == iteration) {
-				return result.toMap()
+			val previousPointMap = pointMap.toMap().toMap
+			formatIteration(viewModel, pointMap)
+			if (pointMap.toMap().toMap == previousPointMap) {
+				return pointMap.toMap()
 			}
 		}
 
-		result.toMap()
+		pointMap.toMap()
+	}
+
+	private def formatIteration(viewModel: ViewModelComponents, pointMap: PointMap) = {
+		for (actor <- viewModel._actors.values) {
+			if (actor.id == 0) {
+				pointMap.put(topLeftCornerIdForActor(actor.id), Fixed2DPoint(LEFT_MARGIN, TOP_MARGIN))
+			} else {
+				pointMap.put(topLeftCornerIdForActor(actor.id),
+					pointMap(topRightCornerIdForActor(actor.id - 1)).right(DISTANCE_BETWEEN_ACTORS))
+			}
+
+			pointMap.put(topRightCornerIdForActor(actor.id),
+				pointMap(topLeftCornerIdForActor(actor.id)).right(painter.preRender(actor).x))
+
+			val actorBox = painter.preRender(actor)
+
+			pointMap.put(bottomMiddleCornerIdForActor(actor.id),
+				pointMap(topLeftCornerIdForActor(actor.id))
+					.right((actorBox.x - 1) / 2)
+					.down(actorBox.y)
+			)
+
+			for (activity <- actor.activities) {
+				if (activity.id == 0) {
+					pointMap.put(topLeftCornerIdForActivity(actor.id, activity.id),
+						pointMap(bottomMiddleCornerIdForActor(actor.id)).left(painter.preRender(activity)))
+					for (point <- activity.rightPoints) {
+						point._2 match {
+							case autoSignal: AutoSignalComponent => {
+								pointMap.put("actor_0_activity_0_right_point_0", Fixed2DPoint(6, 6))
+								pointMap.put("actor_0_activity_0_right_point_1", Fixed2DPoint(6, 9))
+							}
+							case biSignal: BiSignalComponent => {
+								pointMap.put("actor_0_activity_0_right_point_0", Fixed2DPoint(6, 6))
+							}
+						}
+
+					}
+
+					for (point <- activity.leftPoints) {
+						point._2 match {
+							case biSignal: BiSignalComponent => {
+								pointMap.put(pointForActivity(actor.id
+									, activity.id, point._1, "left"),
+									Fixed2DPoint(19, 6))
+							}
+						}
+
+					}
+				}
+			}
+		}
 	}
 
 	private def topLeftCornerIdForActor(column: Int) = s"actor_${column}_top_left"
