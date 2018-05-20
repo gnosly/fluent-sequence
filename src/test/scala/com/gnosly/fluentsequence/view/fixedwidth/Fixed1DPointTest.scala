8fdd3116019ea32f98f747fcd59b19fe39665718
gnosly <fabriziogiovannetti@gmail.com>
2018-05-20 15:14:17 +0200

    added test on Fixed1dPoint

---
+++ src/test/scala/com/gnosly/fluentsequence/view/fixedwidth/Fixed1DPointTest.scala
@@ -0,0 +1,18 @@
+package com.gnosly.fluentsequence.view.fixedwidth
+
+import org.scalatest.{FunSuite, Matchers}
+
+class Fixed1DPointTest extends FunSuite with Matchers{
+
+
+	test("ok") {
+		resolve(Fixed1DPoint(1) + Fixed1DPoint(2)) shouldBe Fixed1DPoint(3)
+		resolve(Fixed1DPoint(2) - Fixed1DPoint(1)) shouldBe Fixed1DPoint(1)
+		Fixed1DPoint(2) < Fixed1DPoint(3) shouldBe true
+		Fixed1DPoint(2) < Fixed1DPoint(2) shouldBe false
+		Fixed1DPoint(2) <=  Fixed1DPoint(2) shouldBe true
+		Fixed1DPoint(2) <=  Fixed1DPoint(1) shouldBe false
+	}
+
+	def resolve(point: Point1d) = point.resolve(null)
+}
