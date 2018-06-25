db92a751063f40f2a6a97d1d685180fbc74517bc
fgiovannetti <fabrizio.giovannetti@rumbo.com>
2018-06-25 13:53:33 +0200

    extracted activity painter

---
+++ src/main/scala/com/gnosly/fluentsequence/view/fixedwidth/painter/FixedWidthActivityPainter.scala
@@ -0,0 +1,45 @@
+package com.gnosly.fluentsequence.view.fixedwidth.painter
+
+import com.gnosly.fluentsequence.view.Coordinates.{Activity, Actor}
+import com.gnosly.fluentsequence.view.fixedwidth.{Fixed2dPoint, FixedWidthCanvas}
+import com.gnosly.fluentsequence.view.model.component.ActivityComponent
+
+class FixedWidthActivityPainter() {
+
+  def paint(activity: ActivityComponent, pointMap: Map[String, Fixed2dPoint]): FixedWidthCanvas = {
+    val canvas = new FixedWidthCanvas()
+
+    if (activity.isFirst()) {
+      val timelineStart = pointMap(Actor.bottomMiddle(activity.actorId))
+      val topLeftActivity = pointMap(Activity.topLeft(activity.actorId, activity.id))
+      0L until topLeftActivity.y - timelineStart.y foreach (i => canvas.write(timelineStart.down(i), "|"))
+    } else {
+      val previousBottomLeftActivity = pointMap(Activity.bottomLeft(activity.actorId, activity.id - 1))
+      val topLeftActivity = pointMap(Activity.topLeft(activity.actorId, activity.id))
+
+      1L until topLeftActivity.y - previousBottomLeftActivity.y foreach (i => canvas.write(previousBottomLeftActivity.down(i), "|"))
+    }
+
+    val existNextActivity = pointMap.contains(Activity.bottomLeft(activity.actorId, activity.id + 1))
+    if (!existNextActivity) { // not exist //todo
+      val bottomLeft = pointMap(Activity.bottomLeft(activity.actorId, activity.id))
+      canvas.write(bottomLeft.down(1).right(1), "|")
+    }
+
+    val topLeftActivity = pointMap(Activity.topLeft(activity.actorId, activity.id))
+    val bottomLeftActivity = pointMap(Activity.bottomLeft(activity.actorId, activity.id))
+
+    canvas.write(topLeftActivity, "_|_")
+
+    val activityStart = topLeftActivity.down(1)
+    for (i <- 0L to bottomLeftActivity.up(1).y - activityStart.y) {
+      canvas.write(activityStart.down(i), "| |")
+    }
+
+    canvas.write(bottomLeftActivity, "|_|")
+
+    canvas
+  }
+
+
+}
--- src/main/scala/com/gnosly/fluentsequence/view/fixedwidth/painter/FixedWidthPainter.scala
+++ src/main/scala/com/gnosly/fluentsequence/view/fixedwidth/painter/FixedWidthPainter.scala
@@ -7,98 +7,66 @@ import com.gnosly.fluentsequence.view.model.component._
 import com.gnosly.fluentsequence.view.{Canvas, Painter}
 
 class FixedWidthPainter extends Painter {
-	val actorPainter = new FixedWidthActorPainter()
-	val biSignalPainter = new FixedWidthBiSignalPainter()
-	val autoSignalPainter = new FixedWidthAutoSignalPainter()
-	val preRenderer = new FixedWidthPreRenderer()
+  val actorPainter = new FixedWidthActorPainter()
+  val biSignalPainter = new FixedWidthBiSignalPainter()
+  val autoSignalPainter = new FixedWidthAutoSignalPainter()
+  val preRenderer = new FixedWidthPreRenderer()
+  val activityPainter = new FixedWidthActivityPainter()
 
-	override def paint(viewModel: ViewModelComponents, pointMap: Map[String, Fixed2dPoint]): Canvas = {
-		val canvas = new FixedWidthCanvas()
-		paintTitle(viewModel, pointMap, canvas)
+  override def paint(viewModel: ViewModelComponents, pointMap: Map[String, Fixed2dPoint]): Canvas = {
+    val titleCanvas = paintTitle(viewModel, pointMap)
 
-		val actorCanvas = viewModel._actors.map(
-			a => actorPainter.paint(a._2, pointMap)
-		)
+    val actorCanvas = viewModel._actors.map(a => actorPainter.paint(a._2, pointMap))
 
-		viewModel._actors.flatMap(_._2.activities).foreach(
-			a => {
-				paint(a, canvas, pointMap)
-			}
-		)
+    val activityCanvas = for {
+      a <- viewModel._actors
+      activity <- a._2.activities
+    } yield activityPainter.paint(activity, pointMap)
 
-		//print right signal
-		val signalCanvas = for {
-			a <- viewModel._actors
-			activity <- a._2.activities
-			rightPoint <- activity.rightPoints
-		} yield rightPoint._2.signalComponent match {
-			case x: AutoSignalComponent => autoSignalPainter.paint(x, pointMap)
-			case x: BiSignalComponent => biSignalPainter.paint(x, pointMap)
-		}
+    val signalCanvas = for {
+      a <- viewModel._actors
+      activity <- a._2.activities
+      rightPoint <- activity.rightPoints
+    } yield rightPoint._2.signalComponent match {
+      case x: AutoSignalComponent => autoSignalPainter.paint(x, pointMap)
+      case x: BiSignalComponent => biSignalPainter.paint(x, pointMap)
+    }
 
-		return (signalCanvas ++ actorCanvas)
-			.reduce(_.merge(_))
-			.merge(canvas)
-	}
+    return (actorCanvas ++ activityCanvas ++ signalCanvas)
+      .reduce(_.merge(_))
+      .merge(titleCanvas)
+  }
 
-	private def paintTitle(viewModel: ViewModelComponents, pointMap: Map[String, Fixed2dPoint], canvas: FixedWidthCanvas) = {
-		val sequenceWidth = allColumnWidth(viewModel, pointMap)
-		val sequenceHeight = pointMap(ViewMatrix.row(viewModel.lastSignalIndex)).x + 3
+  private def paintTitle(viewModel: ViewModelComponents, pointMap: Map[String, Fixed2dPoint]): FixedWidthCanvas = {
+    val canvas = new FixedWidthCanvas()
+    val sequenceWidth = allColumnWidth(viewModel, pointMap)
+    val sequenceHeight = pointMap(ViewMatrix.row(viewModel.lastSignalIndex)).x + 3
 
-		val component = viewModel.sequenceComponents(0)
-		val sequenceTitle = component.name
-		canvas.write(Fixed2dPoint(0, 0), r("_", sequenceTitle.length + 3))
-		canvas.write(Fixed2dPoint(0, 1), s"| ${sequenceTitle} \\")
-		canvas.write(Fixed2dPoint(0, 2), "|" + r("-", sequenceWidth))
-		canvas.write(Fixed2dPoint(0, sequenceHeight), "|" + r("_", sequenceWidth))
+    val component = viewModel.sequenceComponents(0)
+    val sequenceTitle = component.name
+    canvas.write(Fixed2dPoint(0, 0), r("_", sequenceTitle.length + 3))
+    canvas.write(Fixed2dPoint(0, 1), s"| ${sequenceTitle} \\")
+    canvas.write(Fixed2dPoint(0, 2), "|" + r("-", sequenceWidth))
+    canvas.write(Fixed2dPoint(0, sequenceHeight), "|" + r("_", sequenceWidth))
 
-		for (y <- 3 until sequenceHeight.toInt) {
-			canvas.write(Fixed2dPoint(0, y), "|")
-			canvas.write(Fixed2dPoint(sequenceWidth, y), "|")
-		}
-	}
+    for (y <- 3 until sequenceHeight.toInt) {
+      canvas.write(Fixed2dPoint(0, y), "|")
+      canvas.write(Fixed2dPoint(sequenceWidth, y), "|")
+    }
+    canvas
+  }
 
-	def r(pattern: String, count: Long): String =
-		(0 until count.toInt).map(_ => pattern).reduce(_ + _)
+  def r(pattern: String, count: Long): String =
+    (0 until count.toInt).map(_ => pattern).reduce(_ + _)
 
-	private def allColumnWidth(viewModelComponents: ViewModelComponents, pointMap: Map[String, Fixed2dPoint]): Long = {
-		val sequenceWidth = 3
-		//FIXME: move into viewModel
-		val count = viewModelComponents._actors.foldLeft(0L)((z, a) => {
-			z + sequenceWidth + pointMap(ViewMatrix.column(a._2.id)).x
-		})
+  private def allColumnWidth(viewModelComponents: ViewModelComponents, pointMap: Map[String, Fixed2dPoint]): Long = {
+    val sequenceWidth = 3
+    //FIXME: move into viewModel
+    val count = viewModelComponents._actors.foldLeft(0L)((z, a) => {
+      z + sequenceWidth + pointMap(ViewMatrix.column(a._2.id)).x
+    })
 
-		FormatterConstants.LEFT_MARGIN + count
-	}
+    FormatterConstants.LEFT_MARGIN + count
+  }
 
-	private def paint(activity: ActivityComponent, canvas: FixedWidthCanvas, pointMap: Map[String, Fixed2dPoint]): Unit = {
-		if (activity.isFirst()) {
-			val timelineStart = pointMap(Actor.bottomMiddle(activity.actorId))
-			val topLeftActivity = pointMap(Activity.topLeft(activity.actorId, activity.id))
-			0L until topLeftActivity.y - timelineStart.y foreach (i => canvas.write(timelineStart.down(i), "|"))
-		} else {
-			val previousBottomLeftActivity = pointMap(Activity.bottomLeft(activity.actorId, activity.id - 1))
-			val topLeftActivity = pointMap(Activity.topLeft(activity.actorId, activity.id))
-
-			1L until topLeftActivity.y - previousBottomLeftActivity.y foreach (i => canvas.write(previousBottomLeftActivity.down(i), "|"))
-		}
-
-		val existNextActivity = pointMap.contains(Activity.bottomLeft(activity.actorId, activity.id + 1))
-		if (!existNextActivity) { // not exist //todo
-			val bottomLeft = pointMap(Activity.bottomLeft(activity.actorId, activity.id))
-			canvas.write(bottomLeft.down(1).right(1), "|")
-		}
-
-		val topLeftActivity = pointMap(Activity.topLeft(activity.actorId, activity.id))
-		val bottomLeftActivity = pointMap(Activity.bottomLeft(activity.actorId, activity.id))
-
-		canvas.write(topLeftActivity, "_|_")
-
-		val activityStart = topLeftActivity.down(1)
-		for (i <- 0L to bottomLeftActivity.up(1).y - activityStart.y) {
-			canvas.write(activityStart.down(i), "| |")
-		}
-
-		canvas.write(bottomLeftActivity, "|_|")
-	}
 }
