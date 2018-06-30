5a9f1457a89ad8bc0623924982d92e597839c525
gnosly <fabriziogiovannetti@gmail.com>
2018-06-30 17:29:03 +0200

    encoding eventbook

--- src/main/scala/com/gnosly/fluentsequence/core/Actor.scala
+++ src/main/scala/com/gnosly/fluentsequence/core/Actor.scala
@@ -2,8 +2,14 @@ package com.gnosly.fluentsequence.core
 
 case class Actor(actorType: ActorType, name:String)
 
-trait ActorType
+trait ActorType{
+	def name():String
+}
 
-case class SEQUENCE_ACTOR_TYPE() extends ActorType
+case class SEQUENCE_ACTOR_TYPE() extends ActorType {
+	override def name(): String = "SEQUENCE_ACTOR_TYPE"
+}
 
-case class USER_TYPE() extends ActorType
+case class USER_TYPE() extends ActorType {
+	override def name(): String = "USER_TYPE"
+}
