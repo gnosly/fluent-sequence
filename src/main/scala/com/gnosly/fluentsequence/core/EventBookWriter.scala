5c83d0175345975c8ed4b580afa0a8466b65a2bc
gnosly <fabriziogiovannetti@gmail.com>
2018-09-20 00:34:51 +0200

    FIRED event is encoded

--- src/main/scala/com/gnosly/fluentsequence/core/EventBookWriter.scala
+++ src/main/scala/com/gnosly/fluentsequence/core/EventBookWriter.scala
@@ -11,6 +11,8 @@ class EventBookWriter {
     case s: DONE             => s"DONE|${s.who.actorType.name}|${s.who.name}|${s.something}"
     case s: CALLED =>
       s"CALLED|${s.who.actorType.name}|${s.who.name}|${s.something}|${s.toSomebody.actorType.name}|${s.toSomebody.name}"
+    case s: FIRED =>
+      s"FIRED|${s.who.actorType.name}|${s.who.name}|${s.something}|${s.toSomebody.actorType.name}|${s.toSomebody.name}"
     case s: REPLIED =>
       s"REPLIED|${s.who.actorType.name}|${s.who.name}|${s.something}|${s.toSomebody.actorType.name}|${s.toSomebody.name}"
     case s: NEW_SEQUENCE_SCHEDULED => s"NEW_SEQUENCE_SCHEDULED|${s.who.actorType.name}|${s.who.name}|${s.sequence}"
