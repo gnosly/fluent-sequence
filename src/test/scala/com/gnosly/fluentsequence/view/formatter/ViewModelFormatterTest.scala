16db4d4b01b07d5bad217ef5865c959d75960a58
gnosly <fabriziogiovannetti@gmail.com>
2018-09-28 21:48:05 +0200

    added test on formatter

---
+++ src/test/scala/com/gnosly/fluentsequence/view/formatter/ViewModelFormatterTest.scala
@@ -0,0 +1,67 @@
+package com.gnosly.fluentsequence.view.formatter
+import com.gnosly.fluentsequence.view.model.ViewModelComponents
+import com.gnosly.fluentsequence.view.model.component._
+import com.gnosly.fluentsequence.view.model.point.Fixed2dPoint
+import org.scalatest.{FunSuite, Matchers}
+
+import scala.collection.immutable.HashMap
+import scala.collection.mutable
+
+class ViewModelFormatterTest extends FunSuite with Matchers {
+
+  test("format") {
+    val call = new SyncRequest("call", 0, 0, 0, 1, 0)
+    val response = new SyncResponse("response", 1, 1, 0, 0, 0)
+
+    val userRightPoints = mutable.TreeMap[Int, RightPoint](
+      0 -> ActivityPointForBiSignalOnTheRight(0, call),
+      1 -> ActivityPointForBiSignalOnTheRight(1, response)
+    )
+
+    val userComponent =
+      new ActorComponent(0, "user", asBuffer(new ActivityComponent(0, 0, 0, 1, rightPoints = userRightPoints)))
+
+    val systemRightPoints = mutable.TreeMap[Int, LeftPoint](
+      0 -> ActivityPointForBiSignalOnTheLeft(0, call),
+      1 -> ActivityPointForBiSignalOnTheLeft(1, response)
+    )
+
+    val systemComponent =
+      new ActorComponent(1, "system", asBuffer(new ActivityComponent(0, 0, 0, 1, leftPoints = systemRightPoints)))
+
+    val viewModel = ViewModelComponents(mutable.HashMap("user" -> userComponent, "system" -> systemComponent))
+
+    val formatter = new ViewModelFormatter(new FixedPreRenderer())
+
+    val pointMap: Map[String, Fixed2dPoint] = formatter.format(viewModel)
+
+    pointMap shouldBe HashMap(
+      "actor_0_activity_0_bottom_left" -> Fixed2dPoint(4, 13),
+      "actor_0_activity_0_right_point_0_start" -> Fixed2dPoint(7, 8),
+      "actor_0_activity_0_right_point_0_end" -> Fixed2dPoint(7, 10),
+      "actor_0_activity_0_top_left" -> Fixed2dPoint(4, 7),
+      "actor_0_activity_0_top_right" -> Fixed2dPoint(6, 7),
+      "actor_0_top_left" -> Fixed2dPoint(2, 3),
+      "actor_0_activity_0_right_point_1_end" -> Fixed2dPoint(7, 13),
+      "actor_0_bottom_middle" -> Fixed2dPoint(5, 7),
+      "actor_0_activity_0_right_point_1_start" -> Fixed2dPoint(7, 11),
+      "actor_0_top_right" -> Fixed2dPoint(10, 3),
+      "actor_1_top_right" -> Fixed2dPoint(30, 3),
+      "actor_1_activity_0_left_point_0_start" -> Fixed2dPoint(0, 1),
+      "actor_1_activity_0_left_point_0_end" -> Fixed2dPoint(0, 3),
+      "actor_1_top_left" -> Fixed2dPoint(20, 3),
+      "actor_1_activity_0_left_point_1_start" -> Fixed2dPoint(0, 11),
+      "actor_1_activity_0_left_point_1_end" -> Fixed2dPoint(0, 13),
+      "actor_1_bottom_middle" -> Fixed2dPoint(24, 7),
+      "row_0" -> Fixed2dPoint(10, 0),
+      "row_1" -> Fixed2dPoint(13, 0),
+      "column_0" -> Fixed2dPoint(18, 0),
+      "column_1" -> Fixed2dPoint(5, 0)
+    )
+
+  }
+
+  private def asBuffer(component: ActivityComponent): mutable.Buffer[ActivityComponent] = {
+    mutable.Buffer[ActivityComponent](component)
+  }
+}
