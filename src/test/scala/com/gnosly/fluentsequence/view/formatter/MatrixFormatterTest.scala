52d34b268d9bd0dc17ed77ea9c2b77d26f3d0e4e
gnosly <fabriziogiovannetti@gmail.com>
2018-09-30 19:54:57 +0200

    trying to clean the test

--- src/test/scala/com/gnosly/fluentsequence/view/formatter/MatrixFormatterTest.scala
+++ src/test/scala/com/gnosly/fluentsequence/view/formatter/MatrixFormatterTest.scala
@@ -1,5 +1,6 @@
 package com.gnosly.fluentsequence.view.formatter
 import com.gnosly.fluentsequence.view.formatter.FixedPreRenderer.ACTOR_PADDING
+import com.gnosly.fluentsequence.view.formatter.FixedPreRenderer.AUTO_SIGNAL_FIXED_PADDING
 import com.gnosly.fluentsequence.view.formatter.FormatterConstants.DISTANCE_BETWEEN_ACTORS
 import com.gnosly.fluentsequence.view.model.Coordinates
 import com.gnosly.fluentsequence.view.model.Coordinates.Activity
@@ -13,10 +14,12 @@ import scala.collection.mutable
 
 class MatrixFormatterTest extends FunSuite with Matchers {
   val ACTOR_NAME = "user"
+  val SIGNAL_NAME = "SIGNAL NAME"
   val ACTOR_ID = 0
   val ANOTHER_ACTOR_ID = 1
   val ACTIVITY_ID = 0
   val NOT_IMPORTANT = 0
+  val FIRST_INDEX = 0
 
   val formatter = new MatrixFormatter(new FixedPreRenderer)
 
@@ -39,27 +42,15 @@ class MatrixFormatterTest extends FunSuite with Matchers {
   }
 
   test("column where actor has got different signals") {
-    val SIGNAL_NAME = "call"
-    val FIRST_INDEX = 0
+
     val call = new SyncRequest(SIGNAL_NAME, FIRST_INDEX, ACTOR_ID, ACTIVITY_ID, ANOTHER_ACTOR_ID, NOT_IMPORTANT)
 
-    val userRightPoints = mutable.TreeMap[Int, RightPoint](
-      FIRST_INDEX -> ActivityPointForBiSignalOnTheRight(FIRST_INDEX, call),
-    )
-
-    val actor = new ActorComponent(
-      ACTOR_ID,
-      ACTOR_NAME,
-      asBuffer(
-        new ActivityComponent(ACTIVITY_ID, ACTOR_ID, NOT_IMPORTANT, NOT_IMPORTANT, rightPoints = userRightPoints)),
-      isLast = false)
-
-    val matrixPoint = formatter.format(actor)
+    val matrixPoint = formatter.format(actorWith(ActivityPointForBiSignalOnTheRight(FIRST_INDEX, call)))
 
     val actorStartX = new ReferencePoint(Coordinates.Actor.topLeft(ACTOR_ID)).x
     val signalStartX = new ReferencePoint(Activity.pointStart(ACTOR_ID, ACTIVITY_ID, FIRST_INDEX, "right")).x
-    val columnWidthForcedBySignal = signalStartX - actorStartX + Fixed1DPoint(
-      SIGNAL_NAME.length + FixedPreRenderer.BISIGNAL_FIXED_PADDING)
+    val signalWidth = Fixed1DPoint(SIGNAL_NAME.length + FixedPreRenderer.BISIGNAL_FIXED_PADDING)
+    val columnWidthForcedBySignal = signalStartX - actorStartX + signalWidth
 
     matrixPoint shouldBe MatrixPoint(
       Fixed1DPoint(ACTOR_PADDING + ACTOR_NAME.length + DISTANCE_BETWEEN_ACTORS) max columnWidthForcedBySignal
@@ -67,12 +58,24 @@ class MatrixFormatterTest extends FunSuite with Matchers {
   }
 
   test("column where actor has got auto signals") {
-    val SIGNAL_NAME = "something"
-		val FIRST_INDEX = 0
+
     val somethingSignal = new AutoSignalComponent(SIGNAL_NAME, ACTOR_ID, ACTOR_ID, ACTIVITY_ID)
 
+    val matrixPoint = formatter.format(actorWith(ActivityPointLoopOnTheRight(FIRST_INDEX, somethingSignal)))
+
+    val actorStartX = new ReferencePoint(Coordinates.Actor.topLeft(ACTOR_ID)).x
+    val signalStartX = new ReferencePoint(Activity.pointStart(ACTOR_ID, ACTIVITY_ID, FIRST_INDEX, "right")).x
+    val signalWidth = Fixed1DPoint(SIGNAL_NAME.length + AUTO_SIGNAL_FIXED_PADDING)
+    val columnWidthForcedBySignal = signalStartX - actorStartX + signalWidth
+
+    matrixPoint shouldBe MatrixPoint(
+      Fixed1DPoint(ACTOR_PADDING + ACTOR_NAME.length + DISTANCE_BETWEEN_ACTORS) max columnWidthForcedBySignal
+    )
+  }
+
+  private def actorWith(rightPoint: RightPoint) = {
     val userRightPoints = mutable.TreeMap[Int, RightPoint](
-			FIRST_INDEX -> ActivityPointLoopOnTheRight(FIRST_INDEX, somethingSignal),
+      rightPoint.signalComponent.currentIndex -> rightPoint,
     )
 
     val actor = new ActorComponent(
@@ -81,20 +84,11 @@ class MatrixFormatterTest extends FunSuite with Matchers {
       asBuffer(
         new ActivityComponent(ACTIVITY_ID, ACTOR_ID, NOT_IMPORTANT, NOT_IMPORTANT, rightPoints = userRightPoints)),
       isLast = false)
-
-    val matrixPoint = formatter.format(actor)
-
-    val actorStartX = new ReferencePoint(Coordinates.Actor.topLeft(ACTOR_ID)).x
-    val signalStartX = new ReferencePoint(Activity.pointStart(ACTOR_ID, ACTIVITY_ID, FIRST_INDEX, "right")).x
-    val columnWidthForcedBySignal = signalStartX - actorStartX + Fixed1DPoint(
-      SIGNAL_NAME.length + FixedPreRenderer.AUTO_SIGNAL_FIXED_PADDING)
-
-    matrixPoint shouldBe MatrixPoint(
-      Fixed1DPoint(ACTOR_PADDING + ACTOR_NAME.length + DISTANCE_BETWEEN_ACTORS) max columnWidthForcedBySignal
-    )
+    actor
   }
 
   private def asBuffer(component: ActivityComponent): mutable.Buffer[ActivityComponent] = {
     mutable.Buffer[ActivityComponent](component)
   }
+
 }
