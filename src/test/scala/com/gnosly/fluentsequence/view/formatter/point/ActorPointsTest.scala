f80a495ef603e4329462172b5f3bd71a65ffc8dc
fgiovannetti <fabrizio.giovannetti@rumbo.com>
2018-10-04 18:39:00 +0200

    clearer

--- src/test/scala/com/gnosly/fluentsequence/view/formatter/point/ActorPointsTest.scala
+++ src/test/scala/com/gnosly/fluentsequence/view/formatter/point/ActorPointsTest.scala
@@ -2,22 +2,20 @@ package com.gnosly.fluentsequence.view.formatter.point
 
 import com.gnosly.fluentsequence.view.model.Box
 import com.gnosly.fluentsequence.view.model.Coordinates._
-import com.gnosly.fluentsequence.view.model.point.Fixed1DPoint
-import com.gnosly.fluentsequence.view.model.point.Fixed2dPoint
-import com.gnosly.fluentsequence.view.model.point.PointMap
-import com.gnosly.fluentsequence.view.model.point.Variable2DPoint
-import org.scalatest.FunSuite
-import org.scalatest.Matchers
+import com.gnosly.fluentsequence.view.model.point.{Fixed1DPoint, Fixed2dPoint, PointMap, Variable2DPoint}
+import org.scalatest.{FunSuite, Matchers}
 
 class ActorPointsTest extends FunSuite with Matchers {
-  val BOX_WIDTH = 12
+  val ACTOR_ID = 0
+  val TOP_LEFT = Variable2DPoint(Fixed1DPoint(0), Fixed1DPoint(0))
 
   test("points") {
-    val actorPoints = ActorPoints(0, Variable2DPoint(Fixed1DPoint(0), Fixed1DPoint(0)), Box(5, 5))
+    val actorPoints = ActorPoints(ACTOR_ID, TOP_LEFT, Box(5, 5))
+
     actorPoints.toPoints(new PointMap) shouldBe
-      Actor.topLeft(0) -> Fixed2dPoint(0L, 0L) ::
-        Actor.topRight(0) -> Fixed2dPoint(5L, 0L) ::
-        Actor.bottomMiddle(0) -> Fixed2dPoint(2L, 5L) :: Nil
+        Actor.topLeft(ACTOR_ID) -> TOP_LEFT.resolve(null) ::
+        Actor.topRight(ACTOR_ID) -> Fixed2dPoint(5L, 0L) ::
+        Actor.bottomMiddle(ACTOR_ID) -> Fixed2dPoint(2L, 5L) :: Nil
   }
 
 }
