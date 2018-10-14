3e48abebeb46e0fed7a38d5ff7f31ef0bae7c374
gnosly <fabriziogiovannetti@gmail.com>
2018-10-14 16:53:41 +0200

    cleaned actor test

--- src/test/scala/com/gnosly/fluentsequence/view/formatter/ActorFormatterTest.scala
+++ src/test/scala/com/gnosly/fluentsequence/view/formatter/ActorFormatterTest.scala
@@ -4,42 +4,59 @@ import com.gnosly.fluentsequence.view.formatter.FixedPreRenderer.ACTOR_PADDING
 import com.gnosly.fluentsequence.view.formatter.FormatterConstants.DISTANCE_BETWEEN_ACTORS
 import com.gnosly.fluentsequence.view.formatter.FormatterConstants.LEFT_MARGIN
 import com.gnosly.fluentsequence.view.formatter.FormatterConstants.TOP_MARGIN
-import com.gnosly.fluentsequence.view.formatter.point.ActorPoints
-import com.gnosly.fluentsequence.view.model.Box
 import com.gnosly.fluentsequence.view.model.Coordinates.Actor
 import com.gnosly.fluentsequence.view.model.Coordinates.ViewMatrix
 import com.gnosly.fluentsequence.view.model.ViewModels.ActorModel
-import com.gnosly.fluentsequence.view.model.point.Fixed1DPoint
-import com.gnosly.fluentsequence.view.model.point.Reference1DPoint
-import com.gnosly.fluentsequence.view.model.point.ReferencePoint
-import com.gnosly.fluentsequence.view.model.point.Variable2DPoint
+import com.gnosly.fluentsequence.view.model.point._
 import org.scalatest.FunSuite
 import org.scalatest.Matchers
 
 class ActorFormatterTest extends FunSuite with Matchers {
   val ACTOR_ID = 0
   val ACTOR_NAME = "user"
+  val LAST_ACTOR = true
+
   val formatter = new ActorFormatter(new FixedPreRenderer)
 
   test("format first actor alone") {
 
-    formatter.format(ActorModel(ACTOR_ID, ACTOR_NAME, true)) shouldBe
-      ActorPoints(ACTOR_ID,
-                  new Variable2DPoint(LEFT_MARGIN, TOP_MARGIN),
-                  Box(ACTOR_NAME.length + ACTOR_PADDING, ACTOR_PADDING))
+    val points = formatter
+      .format(ActorModel(ACTOR_ID, ACTOR_NAME, LAST_ACTOR))
+      .toPoints(ResolvedPoints(Map()))
+
+    val expectedTopLeft = Fixed2dPoint(LEFT_MARGIN, TOP_MARGIN)
+    val expectedTopRight = expectedTopLeft.right(ACTOR_NAME.length + ACTOR_PADDING)
+    val expectedBottomMiddle = expectedTopLeft.right((ACTOR_NAME.length + ACTOR_PADDING - 1) / 2).down(ACTOR_PADDING)
+
+    points shouldBe List(
+      Actor.topLeft(ACTOR_ID) -> expectedTopLeft,
+      Actor.topRight(ACTOR_ID) -> expectedTopRight,
+      Actor.bottomMiddle(ACTOR_ID) -> expectedBottomMiddle
+    )
   }
 
-  test("format second actor alone") {
+  test("format second actor when before is nearer than DISTANCE_BETWEEN_ACTORS") {
     val ACTOR_ID = 1
     val PREVIOUS_ACTOR_ID = 0
+    val PREVIOUS_ACTOR_TOP_LEFT_X = 5
+    val PREVIOUS_COLUMN_WIDTH = DISTANCE_BETWEEN_ACTORS - 3
+    val NOT_IMPORTANT = 0
 
-    formatter.format(ActorModel(ACTOR_ID, ACTOR_NAME, true)) shouldBe
-      ActorPoints(
-        ACTOR_ID,
-        new ReferencePoint(Actor.topLeft(PREVIOUS_ACTOR_ID))
-          .right(Reference1DPoint(ViewMatrix.column(PREVIOUS_ACTOR_ID)) max Fixed1DPoint(DISTANCE_BETWEEN_ACTORS)),
-        Box(ACTOR_NAME.length + ACTOR_PADDING, ACTOR_PADDING)
-      )
+    val points = formatter
+      .format(ActorModel(ACTOR_ID, ACTOR_NAME, LAST_ACTOR))
+      .toPoints(ResolvedPoints(Map(
+        Actor.topLeft(PREVIOUS_ACTOR_ID) -> Fixed2dPoint(PREVIOUS_ACTOR_TOP_LEFT_X, TOP_MARGIN),
+        ViewMatrix.column(PREVIOUS_ACTOR_ID) -> Fixed2dPoint(PREVIOUS_COLUMN_WIDTH, NOT_IMPORTANT)
+      )))
+
+    val expectedTopLeft = Fixed2dPoint(PREVIOUS_ACTOR_TOP_LEFT_X + DISTANCE_BETWEEN_ACTORS, TOP_MARGIN)
+    val expectedTopRight = expectedTopLeft.right(ACTOR_NAME.length + ACTOR_PADDING)
+    val expectedBottomMiddle = expectedTopLeft.right((ACTOR_NAME.length + ACTOR_PADDING - 1) / 2).down(ACTOR_PADDING)
+
+    points shouldBe List(
+      Actor.topLeft(ACTOR_ID) -> expectedTopLeft,
+      Actor.topRight(ACTOR_ID) -> expectedTopRight,
+      Actor.bottomMiddle(ACTOR_ID) -> expectedBottomMiddle
+    )
   }
-
 }
