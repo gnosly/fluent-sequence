b9b3c0f5408dd0fe1a48a25eaabb9b068c551ee0
gnosly <fabriziogiovannetti@gmail.com>
2018-10-18 22:49:24 +0200

    now we handle backward syncrequest

--- src/main/scala/com/gnosly/fluentsequence/view/svg/painter/SvgSyncRequestPainter.scala
+++ src/main/scala/com/gnosly/fluentsequence/view/svg/painter/SvgSyncRequestPainter.scala
@@ -8,6 +8,14 @@ import com.gnosly.fluentsequence.view.svg.SvgCanvas
 
 class SvgSyncRequestPainter() extends ComponentPainter[SyncRequest] {
   override def paint(biSignal: SyncRequest, pointMap: ResolvedPoints): SvgCanvas = {
+
+    if (biSignal.fromActorId < biSignal.toActorId) {
+      forward(biSignal, pointMap)
+    } else {
+      backward(biSignal, pointMap)
+    }
+  }
+  private def forward(biSignal: SyncRequest, pointMap: ResolvedPoints) = {
     val signalPoint = pointMap(
       Activity.rightPointStart(biSignal.fromActorId, biSignal.fromActivityId, biSignal.currentIndex))
     val leftActivityPoint = pointMap(
@@ -17,5 +25,19 @@ class SvgSyncRequestPainter() extends ComponentPainter[SyncRequest] {
     new SvgCanvas()
       .drawText(signalPoint.right((distance - biSignal.name.length) / 2), biSignal.name)
       .drawRightArrow(signalPoint.down(1).left(1), signalPoint.down(1).right(distance))
+  }
+
+  private def backward(biSignal: SyncRequest, pointMap: ResolvedPoints) = {
+    val signalEndPoint = pointMap(
+      Activity.rightPointStart(biSignal.toActorId, biSignal.fromActivityId, biSignal.currentIndex))
+
+    val signalStartPoint = pointMap(
+      Activity.leftPointStart(biSignal.fromActorId, biSignal.toActivityId, biSignal.currentIndex))
+
+    val distance = Math.abs(signalStartPoint.x - signalEndPoint.x)
+
+    new SvgCanvas()
+      .drawText(signalEndPoint.right((distance - biSignal.name.length) / 2), biSignal.name)
+      .drawLeftArrow(signalEndPoint.down(1).left(1), signalEndPoint.down(1).right(distance))
   }
 }
--- src/test/scala/com/gnosly/fluentsequence/view/svg/painter/SvgSyncRequestPainterTest.scala
+++ src/test/scala/com/gnosly/fluentsequence/view/svg/painter/SvgSyncRequestPainterTest.scala
@@ -15,4 +15,15 @@ class SvgSyncRequestPainterTest extends SyncRequestPainterTest {
 				|</svg>
 			|""".stripMargin
   )
+
+  backwardTest(
+    painter,
+    () =>
+      """<svg xmlns="http://www.w3.org/2000/svg" version="1.1" height="0" width="0">
+				|<text x="40" y="0" font-size="16px" text-anchor="start">sync request</text>
+				|<line x1="-8" y1="10" x2="200" y2="10" style="stroke:black;stroke-width:1.5;" />
+				|<polyline fill="none" stroke="black" stroke-width="1.5" stroke-linecap="square" stroke-linejoin="miter" points="2,5 -8,10 2,15"/>
+				|</svg>
+				|""".stripMargin
+  )
 }
